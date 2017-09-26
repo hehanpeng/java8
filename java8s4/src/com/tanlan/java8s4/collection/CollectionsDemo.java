7862a32703f5421ea71c5c22ce45a75ea518a38b
hehanpeng <123123heyeLove>
2017-09-26 20:55:40 +0800

    2017年9月26日20:55:35

--- java8s4/src/com/tanlan/java8s4/collection/CollectionsDemo.java
+++ java8s4/src/com/tanlan/java8s4/collection/CollectionsDemo.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
+/*
+* Collections 类
+* 对集合数据的操作
+* 转换集合
+* 产生特殊集合
+* 工具类
+* 排序 查询 其他操作
+* */
 public class CollectionsDemo {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<Integer>();//有序对象集合，可以重复，可以存储多个null
-        nums.add(10);
+        nums.add(10);//自动装箱
         nums.add(4);
         nums.add(6);
         nums.add(2);
@@ -17,29 +26,47 @@ public class CollectionsDemo {
             System.out.println(nums.get(i));
         }
 
+        //排序
         Collections.sort(nums);
         nums.forEach(System.out::println);
 
+        //二分查找,前提:应该先排好序
         System.out.println(Collections.binarySearch(nums, 4));
         System.out.println("-------------------");
+        //打乱list
         Collections.shuffle(nums);
         nums.forEach(System.out::println);
         System.out.println("-------------------");
+        //lis反转
         Collections.reverse(nums);
         nums.forEach(System.out::println);
         System.out.println("-------------------");
+        //交换
         Collections.swap(nums, 1, 3);
         nums.forEach(System.out::println);
 
+        /*
+        * 转换集合
+        * unmodifiableXxx:转换为只读集合
+        * synchronizedXxx:转换为同步集合（线程安全）
+        * checkedXxx:转换为受检集合
+        * */
+
+        //受检集合 可以将集合的异常控制在add处，无法insert
         List<Integer> nums3 = Collections.checkedList(nums, Integer.class);
 
         List nums2 = nums3;
-        //nums2.add("ssss");
+        //nums2.add("ssss"); nums2 任意加各种类型
 //		for (Object object : nums2) {
 //			
-//		}
+//		}   出现异常  ！！！
 
+        /*
+        * 产生特殊集合
+        * emptyXxxx：产生空集合
+        * singletonXxx: 产生单例集合
+        * */
         List<String> name = Collections.singletonList("Tom");
-        name.add("Jack");
+        name.add("Jack");//不支持操作异常   只可存放一个元素
     }
 }
--- java8s4/src/com/tanlan/java8s4/stream/StreamDemo1.java
+++ java8s4/src/com/tanlan/java8s4/stream/StreamDemo1.java
@@ -3,11 +3,18 @@ package com.tanlan.java8s4.stream;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+* 聚合操作是指从一个值的集合中计算出一个单值的结果，该结果可能是基本数据类型，对象（单实体类或集合）或void
+* stream是支持顺序或并行聚合操作的数据元素序列
+* 特点：
+* 无存储、可表示无限元素、基于内部迭代、支持并行处理、支持函数式编程、可以有序或无序、不可重用
+* Datasource-> (filter->map) #中间操作，支持链式调用#  -> reduce #终结，出现最终结果#  java.util.stream
+* */
 public class StreamDemo1 {
 
-	public static void main(String[] args) {
-		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5,6);
-		System.out.println(nums.stream().filter(n->n%2==0).map(n->n*2).reduce(Integer::sum).get());
-	}
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6);
+        System.out.println(nums.stream().filter(n -> n % 2 == 0).map(n -> n * 2).reduce(Integer::sum).get());
+    }
 
 }
