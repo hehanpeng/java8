f96fbfba4eee11ba36de5a6012041abe687b8f0e
hehanpeng <123123heyeLove>
2017-09-28 15:46:09 +0800

    2017年9月28日15:46:05

--- java8s4/src/com/tanlan/java8s4/stream/CollecDemo.java
+++ java8s4/src/com/tanlan/java8s4/stream/CollecDemo.java
@@ -1,42 +1,43 @@
 package com.tanlan.java8s4.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
+/*
+* 在Stream上进行数据收集操作
+* Collect接口与Collectors类
+* Stream提供的方法（2）
+* collect 最终  将Stream中的元素收集到集合或者合适的容器中
+* */
 public class CollecDemo {
 
-	public static void main(String[] args) {
-		List<String> names = Person.persons().stream().map(Person::getName)
-				.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-		System.out.println(names);
+    public static void main(String[] args) {
+        List<String> names = Person.persons().stream().map(Person::getName)
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        System.out.println(names);
 
-		List<String> names1 = Person.persons().stream().map(Person::getName)
-				.collect(Collectors.toList());
-		System.out.println(names1);
+        List<String> names1 = Person.persons().stream().map(Person::getName)
+                .collect(Collectors.toList());
+        System.out.println(names1);
 
-		Set<String> names2 = Person.persons().stream().map(Person::getName)
-				.collect(Collectors.toSet());
-		System.out.println(names2);
+        Set<String> names2 = Person.persons().stream().map(Person::getName)
+                .collect(Collectors.toSet());
+        System.out.println(names2);
 
-		SortedSet<String> names3 = Person.persons().stream()
-				.map(Person::getName)
-				.collect(Collectors.toCollection(TreeSet::new));
-		System.out.println(names3);
-		long num = Person.persons().stream().map(Person::getName)
-				.collect(Collectors.counting());
-		System.out.println(num);
+        SortedSet<String> names3 = Person.persons().stream()
+                .map(Person::getName)
+                .collect(Collectors.toCollection(TreeSet::new));
+        System.out.println(names3);
+        long num = Person.persons().stream().map(Person::getName)
+                .collect(Collectors.counting());
+        System.out.println(num);
 
-		Map<Integer, String> map1 = Person.persons().stream()
-				.collect(Collectors.toMap(Person::getId, Person::getName));
-		System.out.println(map1);
+        Map<Integer, String> map1 = Person.persons().stream()
+                .collect(Collectors.toMap(Person::getId, Person::getName));
+        System.out.println(map1);
 
-		String s = Person.persons().stream().map(Person::getName)
-				.collect(Collectors.joining(","));
-		System.out.println(s);
-	}
+        String s = Person.persons().stream().map(Person::getName)
+                .collect(Collectors.joining(","));
+        System.out.println(s);
+    }
 }
--- java8s4/src/com/tanlan/java8s4/stream/StreamDemo3.java
+++ java8s4/src/com/tanlan/java8s4/stream/StreamDemo3.java
@@ -3,17 +3,32 @@ package com.tanlan.java8s4.stream;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+
+/*
+* Stream 提供的方法（1）
+* 方法     类型   描述
+* peek     中间   返回包含流元素的流，主要用于调试目的 不会对原先的流有影响
+* foreach  最终   对每个元素执行特定的操作
+* map      中间   返回对元素进行运算之后结果的流，一对一
+* filter   中间   根据条件过虑元素
+* distinct 中间   去掉重复元素
+* limit    中间   限制元素的数量
+* skip     中间   丢弃前面一定数量的元素
+* sorted   中间   按照自然排序或比较器排序元素
+* reduce   最终   从流中计算出单个值
+* */
 public class StreamDemo3 {
 
 	public static void main(String[] args) {
 		// int num = Stream.of(1, 2, 3, 4, 5).peek(n ->
 		// System.out.println("one:"+n))
 		// .filter(n -> n % 2 == 1).peek(n -> System.out.println("two:"+n))
-		// .reduce(0, Integer::sum);
+		// .reduce(0, Integer::sum);  初始值0 累加
 		// System.out.println(num);
 
 		// Person.persons().stream().forEach(p->System.out.println(p.getName()));
 		// Person.persons().stream().forEach(System.out::println);
+
 		// Person.persons().stream().map(Person::getName).forEach(System.out::println);
 		// IntStream.range(1, 5).map(n -> n * n).forEach(System.out::println);
 
