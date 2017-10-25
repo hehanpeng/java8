6b70306adbe4af3118a282258865165665529db5
hehanpeng <123123heyeLove>
2017-10-25 11:38:43 +0800

    2017年10月25日11:38:38

--- java8s4/src/com/tanlan/java8s4/io/InputStreamDemo.java
+++ java8s4/src/com/tanlan/java8s4/io/InputStreamDemo.java
@@ -1,20 +1,26 @@
 package com.tanlan.java8s4.io;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
+/*
+* 表示数据源：文件，网络连接
+* 使用该数据源构建输入流
+* 从输入流中读取数据：通常使用循环
+* 关闭输入流：可以使用try-with-resources
+* */
 public class InputStreamDemo {
 
-	public static void main(String[] args) {
-		try (FileInputStream fis = new FileInputStream("f2.txt")) {
-			byte byteData;
-			while ((byteData=(byte) fis.read()) != -1) {
-				System.out.println((char) byteData);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+        //会自动关闭输入流
+        try (FileInputStream fis = new FileInputStream("f2.txt")) {
+            byte byteData;
+            while ((byteData = (byte) fis.read()) != -1) {
+                System.out.println((char) byteData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
 }
